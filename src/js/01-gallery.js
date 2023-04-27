92b90f13adcb9b98daaf8daf85242b712ef282b6
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-04-27 09:21:42 +0200

    3 Method for Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,18 +5,65 @@ import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-const imageMarkup = createMarkup(galleryItems);
-gallery.insertAdjacentHTML('beforeend', imageMarkup);
 
-function createMarkup() {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<li> <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-      </a> </li> `;
-    })
-    .join('');
-}
+//Method 1
+galleryItems.forEach(el => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  const img = document.createElement('img');
+
+  a.classList.add('gallery__link');
+  img.classList.add('gallery__image');
+
+  gallery.append(li);
+  li.append(a);
+  a.append(img);
+
+  a.href = el.original;
+  img.src = el.preview;
+  img.alt = el.description;
+});
+
+//Method 2
+// function galleryCreate() {
+//   let galleryArray = [];
+
+//   galleryItems.forEach(el => {
+//     const { preview, original, description } = el;
+
+//     const li = document.createElement('li');
+//     const a = document.createElement('a');
+//     const img = document.createElement('img');
+
+//     a.classList.add('gallery__link');
+//     img.classList.add('gallery__image');
+
+//     a.setAttribute('href', original);
+//     img.setAttribute('src', preview);
+//     img.setAttribute('alt', description);
+
+//     li.append(a);
+//     a.append(img);
+
+//     galleryArray.push(li);
+//   });
+//   gallery.append(...galleryArray);
+// }
+// galleryCreate();
+
+//Method 3
+// const imageMarkup = createMarkup(galleryItems);
+// gallery.insertAdjacentHTML('beforeend', imageMarkup);
+
+// function createMarkup() {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<li> <a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a> </li> `;
+//     })
+//     .join('');
+// }
 
 let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
